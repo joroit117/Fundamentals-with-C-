9899d492aaaf02153d9f2ebbb06fac4bcb6c7d12
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-06-20 14:26:16 +0300

    Add files via upload

---
+++ 1. Data-Types-and-Variables-Exercise/TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int k = 0; k < n; k++)
+                    {
+                        char a = (char)(97 + i);
+                        char b = (char)(97 + j);
+                        char c = (char)(97 + k);
+                        Console.WriteLine($"{a}{b}{c}");
+                    }
+        }
+    }
+}
---
+++ 1. Data-Types-and-Variables-Exercise/WaterOverflow/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int tank = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                tank += liters;
+                if (tank > 255) { Console.WriteLine("Insufficient capacity!"); tank -= liters; }
+            }
+            Console.WriteLine(tank);
+        }
+    }
+}
