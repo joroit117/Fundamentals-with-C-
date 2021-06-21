95b2115d9c4ebf3b0ed45496be4db3a2aa11edad
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-06-21 11:20:32 +0300

    Add files via upload

---
+++ 0. Data-Types-And-Variables/10.LowerOrUpper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10.LowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            if((int)a > 64 && (int)a < 91)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if((int)a > 96 && (int)a < 123)
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
---
+++ 0. Data-Types-And-Variables/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            bool isPrime = false;
+            for (int i = 1; i <= n; i++)
+{
+                int tempVar = i;
+                while (tempVar > 0)
+{
+                    sum += tempVar % 10;
+                    tempVar = tempVar / 10;
+                }
+                isPrime = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", i, isPrime);
+                sum = 0;
+            }
+        }
+    }
+}
