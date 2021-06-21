5a36c49c2dbcfd282841bc385792d04d68d8cefb
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-06-21 11:22:27 +0300

    Add files via upload

---
+++ 0. Data-Types-And-Variables/4.CenturiesToMinutes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace _4.CenturiesToMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            double temp = 365.2422 * centuries * 100;
+            long days = (long)temp;
+            decimal minutes = days * 24 * 60;
+            Console.WriteLine($"{centuries} centuries = {centuries * 100} years = {days} days = {days * 24} hours = {minutes} minutes");
+        }
+    }
+}
