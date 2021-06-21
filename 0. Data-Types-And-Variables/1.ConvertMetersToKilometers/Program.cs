a50205d60a72bf2a0ccfabf823e127c6f67e7a65
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-06-21 11:23:21 +0300

    Add files via upload

---
+++ 0. Data-Types-And-Variables/1.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1.ConvertMetersToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+            double kilometers = 0;
+            kilometers = meters / 1000;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
