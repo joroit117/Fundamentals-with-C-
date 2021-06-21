9e55045e00ce8b0f79abf3540fa7953912dfaacc
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-06-21 11:21:32 +0300

    Add files via upload

---
+++ 0. Data-Types-And-Variables/7.ConcatNames/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _7.ConcatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            string delimeter = Console.ReadLine();
+            Console.WriteLine("{0}{1}{2}", name1, delimeter, name2);
+        }
+    }
+}
