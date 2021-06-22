69d85fa71d88b7d2cbeec9ab5203b088fa064400
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-06-22 13:38:16 +0300

    Add files via upload

---
+++ 3. Arrays/8.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _8.CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] condensed = new int[nums.Length-1];
+            if(nums.Length < 2)
+            {
+                Console.WriteLine(nums[0]);
+            }
+            else
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    for (int i = 0; i < nums.Length - 1; i++)
+                    {
+                        condensed[i] = nums[i] + nums[i + 1];
+                    }
+                    nums = condensed;
+                }
+                Console.WriteLine(condensed[0]);
+            }
+        }
+    }
+}
