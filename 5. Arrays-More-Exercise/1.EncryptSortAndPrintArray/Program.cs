e853425893a92aec2cfa77c3272f70635e999b32
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-06-24 17:58:36 +0300

    Add files via upload

---
+++ 5. Arrays-More-Exercise/1.EncryptSortAndPrintArray/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _1.EncryptSortAndPrintArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arraySums = new int[n];
+            int counter = 0;
+            for(int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+                for(int j = 0; j < s.Length; j++)
+                {
+                    if ((int)s[j] >= 65 && (int)s[j] <= 90)
+                    {
+                        if(s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U')
+                        {
+                            arraySums[counter] += ((int)s[j] * s.Length);
+                        }
+                        else
+                        {
+                            arraySums[counter] += ((int)s[j] / s.Length);
+                        }
+                    }
+                    else if ((int)s[j] >= 97 && (int)s[j] <= 122)
+                    {
+                        if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
+                        {
+                            arraySums[counter] += ((int)s[j] * s.Length);
+                        }
+                        else
+                        {
+                            arraySums[counter] += ((int)s[j] / s.Length);
+                        }
+                    }
+                }
+                counter++;
+            }
+            SortAndPrint(arraySums, n);
+        }
+        static void SortAndPrint(int[] arr, int n)
+        {
+            for(int i = 0; i < n-1; i++)
+            {
+                for(int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int a = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = a;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ 5. Arrays-More-Exercise/2.PascalTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            long[][] pascalTriangle = new long[lines][];
+            for (var row = 0; row < lines; row++)
+            {
+                pascalTriangle[row] = new long[row + 1];
+                pascalTriangle[row][0] = 1;
+                pascalTriangle[row][^1] = 1;
+
+                for (var col = 1; col < row; col++)
+                {
+                    pascalTriangle[row][col] = pascalTriangle[row - 1][col - 1] + pascalTriangle[row - 1][col];
+                }
+            }
+
+            for (var row = 0; row < lines; row++)
+            {
+                Console.WriteLine(string.Join(" ", pascalTriangle[row]));
+            }
+        }
+    }
+}
