07d3d7e337e60b6dc58294df4c1d50b36a104286
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-06-24 17:56:46 +0300

    Add files via upload

---
+++ 5. Arrays-More-Exercise/3.RecursiveFibonachi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3.RecursiveFibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fib(n));
+        }
+        public static int Fib(int n1)
+        {
+            if (n1 <= 2)
+                return 1;
+            else
+                return Fib(n1 - 1) + Fib(n1 - 2);
+        }
+    }
+}
---
+++ 5. Arrays-More-Exercise/4.FoldAndSum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int k = arr.Length / 4;
+            int[] upper = new int[2 * k];
+            int upperCounter = 0;
+            int[] lower = new int[2 * k];
+            int lowerCounter = 0;
+            for (int i = k - 1; i >= 0; i--)
+            {
+                upper[upperCounter] += arr[i];
+                upperCounter++;
+            }
+            for(int i = arr.Length - 1; i > arr.Length - k - 1; i--)
+            {
+                upper[upperCounter] += arr[i];
+                upperCounter++;
+            }
+            for(int i = k; i < arr.Length - k; i++)
+            {
+                lower[lowerCounter] += arr[i];
+                lowerCounter++;
+            }
+            for(int i = 0; i < upperCounter; i++)
+            {
+                upper[i] += lower[i];
+            }
+            for(int i = 0; i < upperCounter; i++)
+            {
+                Console.Write(upper[i] + " ");
+            }
+        }
+    }
+}
