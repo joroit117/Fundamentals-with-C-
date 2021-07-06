a9590c5246e05dfb9ec5ac708ca22a4910a6d511
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-07-06 12:13:39 +0300

    Add files via upload

---
+++ 6. Methods/5.Orders/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _5.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            TotalPrice(product, quantity);
+        }
+        static void TotalPrice(string p, int q)
+        {
+            double price = 0;
+            switch (p)
+            {
+                case "coffee":
+                    price = 1.5;
+                    break;
+                case "water":
+                    price = 1;
+                    break;
+                case "coke":
+                    price = 1.4;
+                    break;
+                case "snacks":
+                   price = 2;
+                    break;
+            }
+            Console.WriteLine($"{price * q:f2}");
+        }
+    }
+}
---
+++ 6. Methods/7.RepeatString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _7.RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string multiplied = StringMultiplied(s, n);
+            Console.WriteLine(multiplied);
+        }
+        static string StringMultiplied(string s, int n)
+        {
+            string multiplied = "";
+            for(int i = 0; i < n; i++)
+            {
+                multiplied += s;
+            }
+            return multiplied;
+        }
+    }
+}
