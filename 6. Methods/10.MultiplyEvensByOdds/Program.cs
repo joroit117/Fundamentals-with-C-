dbbadb6e6f5fa3d87c89c5b435a1ed4d5db55153
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-07-06 12:12:12 +0300

    Add files via upload

---
+++ 6. Methods/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _10.MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            number = Math.Abs(number);
+            int result = GetMultipleOfEvenAndOdds(number);
+            Console.WriteLine(result);
+        }
+        static int GetSumOfEvenDigits(int n)
+        {
+            int sum = 0;
+            while(n >= 1)
+            {
+                int digit = n % 10;
+                if(digit % 2 == 0)
+                {
+                    sum += digit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+        static int GetSumOfOddDigits(int n)
+        {
+            int sum = 0;
+            while (n >= 1)
+            {
+                int digit = n % 10;
+                if (digit % 2 != 0)
+                {
+                    sum += digit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+        static int GetMultipleOfEvenAndOdds(int n)
+        {
+            int evenSum = GetSumOfEvenDigits(n);
+            int oddSum = GetSumOfOddDigits(n);
+            return evenSum * oddSum;
+        }
+    }
+}
