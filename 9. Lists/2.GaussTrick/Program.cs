af94a1355f62773ca896c6ad21c021bbeabb8eaf
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-07-06 19:59:32 +0300

    Add files via upload

---
+++ 9. Lists/2.GaussTrick/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            for(int i = 0; i < numbers.Count / 2; i++)
+            {
+                int left = numbers[i];
+                int right = numbers[numbers.Count - 1 - i];
+
+                int element = left + right;
+
+                result.Add(element);
+            }
+
+            if(numbers.Count % 2 == 1)
+            {
+                int midElement = numbers[numbers.Count / 2];
+                result.Add(midElement);
+            }
+
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
---
+++ 9. Lists/3.MergingListss/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.MergingListss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers1 = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> numbers2 = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> result = new List<int>();
+            int resLenght = 0;
+            bool isFirstSmaller = false;
+            if(numbers1.Count < numbers2.Count)
+            {
+                resLenght = numbers1.Count;
+                isFirstSmaller = true;
+            }
+            else
+            {
+                resLenght = numbers2.Count;
+                isFirstSmaller = false;
+            }
+            for(int i = 0; i < resLenght; i++)
+            {
+                result.Add(numbers1[i]);
+                result.Add(numbers2[i]);
+            }
+            if(isFirstSmaller)
+            {
+                for(int i = resLenght; i < numbers2.Count; i++)
+                {
+                    result.Add(numbers2[i]);
+                }
+            }
+            else
+            {
+                for (int i = resLenght; i < numbers1.Count; i++)
+                {
+                    result.Add(numbers1[i]);
+                }
+            }
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
