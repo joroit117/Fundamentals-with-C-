f44e44dca8f3ce5356ae886fcabe3ec690871cc7
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-07-23 12:15:19 +0300

    Add files via upload

---
+++ 13. AssociativeArrays/5.WordFilter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _5.WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split()
+                .Where(w => w.Length % 2 == 0)
+                .ToArray();
+
+            Console.WriteLine(string.Join("\n", words));
+        }
+    }
+}
