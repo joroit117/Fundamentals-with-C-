8eafb9ee51c73b3b3d9790379a8ee32a3f9b2307
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-08-02 14:04:29 +0300

    Add files via upload

---
+++ 14. AssociativeArrays-Exercise/7.StudentAcademy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> students = new Dictionary<string, double>();
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if(students.ContainsKey(studentName))
+                {
+                    students[studentName] = (students[studentName] + studentGrade) / 2.00;
+                }
+                else
+                {
+                    students.Add(studentName, studentGrade);
+                }
+            }
+            foreach(var student in students.OrderByDescending(x => x.Value))
+            {
+                if(student.Value >= 4.50)
+                {
+                    Console.WriteLine($"{student.Key} -> {student.Value:f2}");
+                }
+            }
+        }
+    }
+}
