8acc540cf22910f0e009fcb2568481cdd93dfe84
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-08-05 13:43:26 +0300

    Add files via upload

---
+++ 15.TextProcessing/4.TextFilter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _4.TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannnedWords = Console.ReadLine().Split(", ");
+
+            string text = Console.ReadLine();
+
+            while(true)
+            {
+                int index = 0;
+                foreach (var word in bannnedWords)
+                {
+                    index = text.IndexOf(word);
+                    if(index == -1)
+                    {
+                        break;
+                    }
+                    string asterisk = new string('*', word.Length);
+                    text = text.Replace(word, asterisk);
+                }
+                if(index == -1)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ 15.TextProcessing/5.Digits,LettersAndOther/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _5.Digits_LettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string digits = "";
+            string leters = "";
+            string symbols = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(Char.IsDigit(input[i]))
+                {
+                    digits += input[i];
+                }
+                else if(Char.IsLetter(input[i]))
+                {
+                    leters += input[i];
+                }
+                else
+                {
+                    symbols += input[i];
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(leters);
+            Console.WriteLine(symbols);
+        }
+    }
+}
