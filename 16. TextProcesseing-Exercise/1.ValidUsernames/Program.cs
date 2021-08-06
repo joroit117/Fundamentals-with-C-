d01565decb4d88b55919f08e3367c6c93fbfa2ae
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-08-06 13:04:08 +0300

    Add files via upload

---
+++ 16. TextProcesseing-Exercise/1.ValidUsernames/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1.ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            foreach(var username in usernames)
+            {
+                bool isValid = true;
+                if(username.Length >= 3 && username.Length <= 16)
+                {
+                    for(int i = 0; i < username.Length; i++)
+                    {
+                        if(Char.IsDigit(username[i]) || Char.IsLetter(username[i]) || username[i] == '-' || username[i] == '_')
+                        {
+
+                        }
+                        else
+                        {
+                            isValid = false;
+                        }
+                    }
+                }
+                else 
+                {
+                    isValid = false;
+                }
+                if (isValid)
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+    }
+}
---
+++ 16. TextProcesseing-Exercise/2.CharacterMultiplier/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _2.CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int sum = 0;
+            string first = input[0];
+            string second = input[1];
+
+            if (first.Length >= second.Length)
+            {
+                for(int i = 0; i < first.Length; i++)
+                {
+                    if(i < second.Length)
+                    {
+                        sum += (int)first[i] * (int)second[i];
+                    }
+                    else
+                    {
+                        sum += (int)first[i];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < second.Length; i++)
+                {
+                    if (i < first.Length)
+                    {
+                        sum += (int)first[i] * (int)second[i];
+                    }
+                    else
+                    {
+                        sum += (int)second[i];
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
