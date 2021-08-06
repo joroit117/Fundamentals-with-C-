07eb1b4a14c8d3e840dfee8a674aa1e05c33322d
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-08-06 13:03:29 +0300

    Add files via upload

---
+++ 16. TextProcesseing-Exercise/3.ExtractFille/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3.ExtractFille
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+
+            int beginIndex = filePath.LastIndexOf("\\");
+            int endIndex = filePath.IndexOf('.');
+
+            string extension = filePath.Substring(endIndex + 1);
+
+            filePath = filePath.Remove(endIndex);
+
+            string fileName = filePath.Substring(beginIndex + 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+            
+        }
+    }
+}
---
+++ 16. TextProcesseing-Exercise/4.CaesarCipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4.CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = "";
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                int temp = (int)input[i] + 3;
+                output += (char)temp;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
